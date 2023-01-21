6273abc44ead9ac87f62f5f31369f7a6633c195a
tlreacttl <react2023.ttt02@gmail.com>
2023-01-21 23:05:06 +0900

    [入門編1-7分]

---
+++ src/reducks/store/initialState.js
@@ -0,0 +1,10 @@
+const initialState = {
+    users: {
+      isSignedIn: false,
+      uid: "",
+      username: ""
+    }
+  };
+  
+  export default initialState
+  
---
+++ src/reducks/store/store.js
@@ -0,0 +1,22 @@
+import {
+    createStore as reduxCreateStore,
+    combineReducers,
+    applyMiddleware
+  } from 'redux';
+  
+import {UsersReducer} from '../users/reducers';
+import {connectRouter, routerMiddleware} from 'connected-react-router';
+
+
+export default function createStore(history) {
+  return reduxCreateStore(
+    combineReducers({
+        router: connectRouter(history),
+        users: UsersReducer,
+    }),
+    applyMiddleware(
+        routerMiddleware(history)
+    )
+  );
+}
+  
